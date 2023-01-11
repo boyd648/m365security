ecdb74b2d208327b7b67df76296add553b97ff30
John Gruber <gruberjl@gmail.com>
2023-01-11 07:39:01 -0500

    added article

---
+++ src/pages/why-get-it-certified.js
@@ -0,0 +1,23 @@
+module.exports = {
+  title: 'why get your IT certification',
+  description: 'Demonstrate your knowledge and skills in this field to earn an IT certification',
+  canonical: 'why-get-it-certified',
+  ogImage: 'happy-working.jpg',
+  titleimg: 'assets/imgs/happy-working-social.jpg',
+  body: `<p>Information technology (IT) is a rapidly growing field that encompasses a wide range of technologies, including computers, software, networks, and data storage systems. As businesses and organizations continue to rely heavily on technology to drive growth and innovation, the demand for skilled IT professionals continues to rise. One way to demonstrate your knowledge and skills in this field is to earn an IT certification. In this article, we'll discuss some of the key benefits of obtaining an IT certification and why it can be a valuable asset to your career.</p>
+  <p>One of the biggest advantages of obtaining an IT certification is that it can help you stand out in a highly competitive job market. Many job postings in the IT field require specific certifications as a minimum qualification. For example, a company may require that an applicant for a network administrator position hold a Cisco Certified Network Associate (CCNA) certification. By obtaining the certification, you can demonstrate to potential employers that you have the knowledge and skills they are looking for.</p>
+  <p>Another advantage of earning an IT certification is that it can lead to higher earning potential. According to data from the IT trade association CompTIA, certified professionals earn on average 15-20% more than their non-certified counterparts. Additionally, some certifications carry a premium and can result in even higher earning potential.</p>
+  <p>Beyond financial benefits, certification also demonstrates to employers that you are committed to your career, and it can help you to be more competitive in your field. Employers are also better able to trust you with more advanced and critical tasks as you have demonstrated your ability to keep your technical knowledge current and relevant.</p>
+  <p>Additionally, IT certifications can provide you with valuable training and education opportunities. Many certification programs require that you complete a specific course or training program before you can take the certification exam. This training can help you to deepen your knowledge of a particular technology or area of the field, and can also help you to identify areas where you need to improve your skills.</p>
+  <p>In addition to the benefits to your career and earning potential, obtaining IT certifications can also have a positive impact on the organization where you work. When you earn a certification, you gain new knowledge and skills that can be applied to your job. This can help your organization to improve its IT infrastructure and operations, and can ultimately lead to increased productivity and revenue growth.</p>
+  <p>There are also many different types of IT certifications available, making it easy to find one that aligns with your career goals and interests. Some popular certifications include:</p>
+  <ul>
+    <li>Microsoft Certified Security Administrator</li>
+    <li>Cisco Certified Network Associate (CCNA)</li>
+    <li>CompTIA A+</li>
+    <li>Certified Information Systems Security Professional (CISSP)</li>
+    <li>Oracle Certified Professional, Java SE 11 Developer</li>
+  </ul>
+  <p>While some certifications are specific to certain products or vendors (for example the CCNA is specific to Cisco products), others (such as CompTIA A+) are more general in nature.</p>
+  <p>Overall, obtaining an IT certification can be a valuable asset to your career. It can help you to stand out in a competitive job market, lead to higher earning potential, and provide valuable training and education opportunities. Additionally, it can benefit the organization where you work by improving its IT infrastructure and operations. With many different types of certifications available, it's important to research and select the one that aligns best with your career goals and interests.</p>`
+}
