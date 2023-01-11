686b43a47604a266062facbda84a0d8cfabb75d9
John Gruber <gruberjl@gmail.com>
2023-01-11 08:27:36 -0500

    added GA on redirect link

--- src/pages/how-to-manage-defender-for-endpoint-5lif8mrvd.js
+++ src/pages/how-to-manage-defender-for-endpoint-5lif8mrvd.js
@@ -4,7 +4,7 @@ module.exports = {
   canonical: 'how-to-manage-defender-for-endpoint-5lif8mrvd',
   ogImage: 'facebook-Walking-path.jpeg',
   titleimg: 'assets/imgs/Walking-path.jpg',
-  redirect: 'https://www.gitbit.org/course/ms-500/blog/how-to-manage-defender-for-endpoint-5lif8mrvd',
+  redirect: 'https://www.gitbit.org/course/ms-500/blog/how-to-manage-defender-for-endpoint-5lif8mrvd?utm_source=boyd648.github.io&utm_medium=referral&utm_campaign=redirect',
   body: `<h1>0 to hero - the ultimate Microsoft Defender for Endpoint (MDE) guide</h1>
   <p>Microsoft Defender for Endpoint is an endpoint security solution developed by Microsoft. It is part of the Microsoft 365 suite of tools and supplies real-time protection against malware, ransomware, and other threats on endpoints, such as PCs, laptops, and servers.</p>
   <p>To learn more go to <a href="https://www.gitbit.org/course/ms-500/blog/how-to-manage-defender-for-endpoint-5lif8mrvd">0 to hero - the ultimate Microsoft Defender for Endpoint (MDE) guide</a>`
