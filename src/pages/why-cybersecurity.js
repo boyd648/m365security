b62cca6d30ada098b02386c41f3f6081fdfc242b
John Gruber <gruberjl@gmail.com>
2023-01-26 12:57:45 -0500

    added webpage

---
+++ src/pages/why-cybersecurity.js
@@ -0,0 +1,25 @@
+module.exports = {
+  title: 'Why cybersecurity is a great field and now is the time to get into a cybersecurity role',
+  description: 'The field of cybersecurity is a great career choice, and now is an opportune time to pursue a role in this industry.',
+  canonical: 'why-cybersecurity',
+  ogImage: 'why-cyber.jpg',
+  titleimg: 'assets/imgs/why-cyber-1920.jpg',
+  body: `<p>Cybersecurity experts will be in high demand in the future due to an increase in the number of cyber attacks and the growing complexity of technology. As more and more companies and organizations move their operations online and rely on technology, the need for individuals who can protect against cyber threats and secure sensitive information will continue to grow. Additionally, the increasing use of Internet of Things (IoT) devices and the rise of 5G networks will create new vulnerabilities that will need to be addressed by cybersecurity professionals. This means that the need for cyber security experts will continue to increase in the future.</p>
+  <p>There are many different cybersecurity job roles, and the specific roles can vary depending on the organization and the industry. Some examples of common cybersecurity job roles include:</p>
+  <ol>
+    <li>Security Analyst: responsible for identifying and addressing potential security threats to an organization.</li>
+    <li>Penetration Tester: simulates a cyber attack on an organization's systems to identify vulnerabilities.</li>
+    <li>Security Engineer: designs and implements security solutions to protect an organization's systems and networks.</li>
+    <li>Information Security Manager: responsible for developing and implementing security policies and procedures for an organization.</li>
+    <li>Network Security Engineer: responsible for protecting an organization's networks from cyber attacks.</li>
+    <li>Cybersecurity Consultant: advises organizations on how to improve their cybersecurity and protect against threats.</li>
+    <li>Cybercrime Investigator: investigates cybercrime and gathers evidence to be used in legal proceedings.</li>
+    <li>Incident Responder: handles and manages security incidents as they occur</li>
+    <li>Security architect: designs, builds and evaluates security systems</li>
+    <li>Compliance Analyst: ensures an organization's compliance with relevant security regulations and standards.</li>
+  </ol>
+
+    <p>These are just a few examples of the many different cybersecurity job roles available. There are many other roles that can be found in the field of Cybersecurity.</p>
+    <p>To see 26 other job roles, their salaries, and the number of current job postings in the U.S. go to <a href="https://www.gitbit.org/course/ms-500/blog/cyber-security-careers-bhmjh1jcx">26 cyber security careers with average pay and number of jobs available</a>
+  `
+}
